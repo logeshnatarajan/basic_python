adfd7fc6e5bd5b95c5affd7d0bd431c883358f2b
logesh <90084493+logeshnatarajan@users.noreply.github.com>
2022-05-13 21:26:42 +0530

    Update 001.py

--- AreaPython/HelloWorld/list/001.py
+++ AreaPython/HelloWorld/list/001.py
@@ -1,7 +1,9 @@
 items = ["milk", "potato", "tomato", "carrotandbean", "curry"]
+# illerating over thelist
 for item in items:
     print(item)
 print()
+# checking elements in list
 for item in items:
     if item != "carrotandbean":
         print(item)
